56b2a07d81ea450c3ca92c417759344e944b5364
User <user@example.com>
2025-04-08 13:28:08 +0300

    Pencere genişliğini artırma: Genişlik 900 pikselden 1200 piksele çıkarıldı

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class MainWindow(QMainWindow):
     def initUI(self):
         """Kullanıcı arayüzünü başlatır"""
         self.setWindowTitle("Çalışan Takip Sistemi")
-        self.setGeometry(100, 100, 900, 700)
+        self.setGeometry(100, 100, 1200, 700)
         self.setWindowIcon(QIcon("icon.png"))
         
         # Ana widget ve layout
